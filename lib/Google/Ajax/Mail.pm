9d3bfe11dee17ccf37296dc991ea36d4b38f5597
Robert Norris <rob@cataclysm.cx>
2009-12-02 07:57:17 +1100

    basic outline for mail app module

--- lib/Google/Ajax/Mail.pm
+++ lib/Google/Ajax/Mail.pm
@@ -3,6 +3,8 @@ package Google::Ajax::Mail;
 use warnings;
 use strict;
 
+use Params::Validate qw(validate_pos SCALAR);
+
 sub get_service {
     return "mail";
 }
@@ -16,6 +18,39 @@ sub get_service_url {
 
     # XXX check this
     return "https://mail.google.com/";
+}
+
+sub _fetch_and_update {
+}
+
+sub _get_cached_data {
+}
+
+sub get_usage {
+    my $self = shift;
+
+    return $self->_get_cached_data("qu")->[0];
+}
+
+sub get_quota {
+    my $self = shift;
+
+    return $self->_get_cached_data("qu")->[1];
+}
+
+sub get_unread {
+    my $self = shift;
+
+    my ($label) = validate_pos(@_,
+        { type => SCALAR },
+    );
+
+    my $label_data = $self->_get_cached_data("ds");
+    for my $item (@$label_data) {
+        return $item->[1] if $item->[0] eq $label;
+    }
+
+    return;
 }
 
 1;
